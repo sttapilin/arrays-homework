b19b484f29fd039d229048796a8629bfd342e30f
sttapilin <sttapilin@gmail.com>
2025-01-17 22:51:26 +0300

    Created test 1

--- src/main/java/com/edu/DefaultCustomArrayList.java
+++ src/main/java/com/edu/DefaultCustomArrayList.java
@@ -1,51 +1,82 @@
 package com.edu;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class DefaultCustomArrayList<E> implements CustomArrayList<E> {
+    int size = 0;
+    E[] array;
 
     public DefaultCustomArrayList() {
+        array = (E[]) new Object[size];
     }
 
     @Override
     public boolean add(E element) {
+        size++;
+        if (size > array.length) {
+            array = Arrays.copyOf(array, size);
+        }
+        array[size - 1] = element;
         return true;
     }
 
     @Override
     public boolean remove(E element) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(element)) {
+                for (int j = i + 1; j < array.length; j++) {
+                    array[i] = array[j];
+                }
+                array = Arrays.copyOf(array, array.length - 1);
+                return true;
+            }
+        }
         return false;
     }
 
     private void remove(int index) {
+        if (index > 0 || index < array.length) {
+            for (int i = index; i < array.length - 1; i++) {
+                array[i] = array[i + 1];
+                System.out.println(array[i]);
+            }
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return array[index];
     }
 
     @Override
     public int size() {
-        return 0;
+        return array.length;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return array.length == 0;
     }
 
     @Override
     public void clear() {
+        array = (E[]) new Object[0];
     }
 
     @Override
     public boolean contains(E element) {
+        for (E s : array) {
+            if (s.equals(element)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public Iterator<E> iterator() {
+
         return null;
     }
 }
